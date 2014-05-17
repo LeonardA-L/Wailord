e52b58dbc002ac57a2a6f721f20db02ff294c4e6
LeonardA-L <leonard.allain-launay@insa-lyon.fr>
2014-05-17 21:26:35 +0200

    Implemented the whole level lines recognition thing, thanks to opencv

--- src/filRouge/wailord/CameraActivity.java
+++ src/filRouge/wailord/CameraActivity.java
@@ -1,5 +1,9 @@
 package filRouge.wailord;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -7,22 +11,45 @@ import android.graphics.Color;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
 import android.hardware.Camera.PictureCallback;
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
+import org.opencv.*;
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.utils.Converters;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+
 public class CameraActivity extends Activity {
 	private static final String TAG = "CActivity";
+	
+	private GestureDetector mGestureDetector;
+	
+	public static final int CAMERA_WIDTH = 640;
     private Camera mCamera;
     private CameraPreview mPreview;
     
@@ -34,6 +61,16 @@ public class CameraActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
 
+     // Supposed to handle autofocus
+        // The class is in the UserDefinedTarget sample
+        //mGestureDetector = new GestureDetector(this, new GestureListener());
+        
+        Log.i(TAG, "Trying to load OpenCV library");
+        if (!OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_2, this, mOpenCVCallBack))
+        {
+          Log.e(TAG, "Cannot connect to OpenCV Manager");
+        }
+        
         img = (ImageView)(findViewById(R.id.imageView1));
         // Create an instance of Camera
         mCamera = getCameraInstance();
@@ -48,6 +85,24 @@ public class CameraActivity extends Activity {
 	        Log.d(TAG, "Preview Layout : "+preview);
 	        preview.addView(mPreview);
 	        
+	        int width = mCamera.getParameters().getPictureSize().width;
+	         int height = mCamera.getParameters().getPictureSize().height;
+	         Log.d(TAG,"Original Camera Size : " +height+" : "+ mCamera.getParameters().getPictureFormat());
+	         double fac = (CAMERA_WIDTH/(double)width);
+	         //double fac = 1;
+	         width = CAMERA_WIDTH;
+	         height*=fac;
+	         Log.d(TAG,"New Camera Size : " +height);
+	        
+	         Parameters params = mCamera.getParameters();
+	         params.setPictureSize(width, height);
+	         //params.set("jpeg-quality", 35);
+	         mCamera.setParameters(params);
+	        
+	         mCamera.startPreview();
+	        
+	         mCamera.autoFocus(null);
+	         
 	     // Add a listener to the Capture button
 	        Button captureButton = (Button) findViewById(R.id.button_capture);
 	        captureButton.setOnClickListener(
@@ -94,16 +149,166 @@ public class CameraActivity extends Activity {
 
         @Override
         public void onPictureTaken(byte[] data, Camera camera) {
+        	Log.d(TAG,"Picture taken");
+        	//Bitmap picture = BitmapFactory.decodeByteArray(data, 0, data.length);
+        	Bitmap picture= BitmapFactory.decodeResource(getResources(), R.drawable.ihem4);
         	
-        	Bitmap picture = BitmapFactory.decodeByteArray(data, 0, data.length);
-        	setImg(picture);
+        	
+        	// Find Contours via OpenCV
+        	Mat image = new Mat(picture.getWidth(),picture.getHeight(), CvType.CV_8UC4,new Scalar(4));
+        	Mat ITimage = new Mat(picture.getWidth(),picture.getHeight(), CvType.CV_8UC4,new Scalar(4));;
+        	Utils.bitmapToMat(picture, image);
 
+        	Imgproc.cvtColor(image, image, Imgproc.COLOR_RGB2GRAY);
+
+        	List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+        	
+        	Imgproc.Canny(image, ITimage, 80, 100);
+        	Mat hierarchy = new Mat();
+        	Imgproc.findContours(ITimage, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+        	
+        	hierarchy.release();
+        	
+        	Imgproc.cvtColor(image, image, Imgproc.COLOR_GRAY2BGR);
+        		
+        	// Get a list of contour-related data
+        	int[][] v = contoursToLists(contours);
+        	// Sort the contour list according to the data
+        	contours = sort(contours, v);
+        	
+        	// Spreading threshold
+        	int currentDiag = -1;
+        	int tresh = 20;
+        	
+        	// Find who contains who (define level)
+        	int[] levelTab = levels(v, tresh);
+        	
+        	// Get max level
+        	int max = -1;
+        	for(int i=0;i<levelTab.length;i++){
+        		if(max == -1 || levelTab[i] > max){
+        			max = levelTab[i];
+        		}
+        	}
+        	
+        	// Draw the contours on the image
+        	for(int i=0;i<contours.size();i++){
+        		
+        		int fac = (int)((levelTab[i])*(255.0/(max))+1);
+        		if(currentDiag == -1 || Math.abs(v[i][1] - currentDiag) >= tresh){
+        			currentDiag = v[i][1];
+        			Imgproc.drawContours(image, contours, i, new Scalar(fac,fac,fac), 3); //#4 square (blue)
+        		}
+        		
+        	}
+        	
+        	// Get back to picture bitmap
+        	Utils.matToBitmap(image, picture);
+        	setImg(picture);
+        
         }
     };
     
+    public int[] levels(int[][] values, int tresh){
+    	int[] levels = new int[values.length];
+    	for(int i =0;i<values.length;i++){
+    		int level = 1;
+    		for(int j=0;j<i;j++){
+    			if(values[j][2] < values[i][2] && values[j][3] < values[i][3] && values[j][4] > values[i][4] && values[j][5] > values[i][5] && Math.abs(values[i][1] - values[j][1]) >= tresh){
+    				level ++;
+    			}
+    		}
+    		levels[i] = level;
+    	}
+    	return levels;
+    }
+    
+    public int[][] contoursToLists(List<MatOfPoint> contours){
+    	int[][] liste = new int[contours.size()][6];
+    	for(int i=0;i<contours.size();i++){
+    		List<org.opencv.core.Point> cont = contours.get(i).toList();
+    		Point min = new Point(-1,-1);
+    		Point max = new Point(-1,-1);
+    		for(int j=0;j<cont.size();j++){
+    			org.opencv.core.Point p = cont.get(j);
+    			if(min.x == -1 || p.x < min.x){
+    				min.x = (int)p.x;
+    			}
+    			if(min.y == -1 || p.y < min.y){
+    				min.y = (int)p.y;
+    			}
+    			
+    			if(max.x == -1 || p.x > max.x){
+    				max.x = (int)p.x;
+    			}
+    			if(max.y == -1 || p.y > max.y){
+    				max.y = (int)p.y;
+    			}
+    		}
+    		
+    		int diag = (int)Math.sqrt(Math.pow(max.x - min.x,2)+ Math.pow(max.y - min.y,2));
+    		//Log.d(TAG," "+i+" : "+diag);
+    		liste[i][0] = i;
+    		liste[i][1] = diag;
+    		liste[i][2] = min.x;
+    		liste[i][3] = min.y;
+    		liste[i][4] = max.x;
+    		liste[i][5] = max.y;
+    	}
+    	
+    	return liste;
+    }
+    
+    public List<MatOfPoint> sort(List<MatOfPoint> contours, int[][] values){
+    	List<MatOfPoint> sorted = new ArrayList<MatOfPoint>();
+    	boolean swapped = true;
+    	while(swapped){
+    		swapped = false;
+    		for(int i=0;i<values.length -1;i++){
+    			int[] tmp = new int[6];
+    			
+    			if(values[i+1][1] > values[i][1]){
+    				tmp[0] = values[i+1][0];
+    				tmp[1] = values[i+1][1];
+    				tmp[2] = values[i+1][2];
+    				tmp[3] = values[i+1][3];
+    				tmp[4] = values[i+1][4];
+    				tmp[5] = values[i+1][5];
+    				
+    				values[i+1][0] = values[i][0];
+    				values[i+1][1] = values[i][1];
+    				values[i+1][2] = values[i][2];
+    				values[i+1][3] = values[i][3];
+    				values[i+1][4] = values[i][4];
+    				values[i+1][5] = values[i][5];
+    				
+    				values[i][0] = tmp[0];
+    				values[i][1] = tmp[1];
+    				values[i][2] = tmp[2];
+    				values[i][3] = tmp[3];
+    				values[i][4] = tmp[4];
+    				values[i][5] = tmp[5];
+    				swapped = true;
+    			}
+    		}
+    	}
+    	
+    	int tres = 10;
+    	
+    	for(int i=0;i< values.length;i++){
+    		if(values[i][1] > tres){
+    			sorted.add(contours.get(values[i][0]));
+    		}
+    	}
+    	
+    	return sorted;
+    }
+    
+
+    
     public void setImg (Bitmap newImage){
     	toDisplay = newImage;
-    	toDisplay = toBinary(toGrayscale(toDisplay));
+    	//toDisplay = toBinary(toGrayscale(toDisplay));
     	img.setImageBitmap(this.toDisplay);
     }
     
@@ -148,4 +353,24 @@ public class CameraActivity extends Activity {
         c.drawBitmap(bmpOriginal, 0, 0, paint);
         return bmpGrayscale;
     }
+    
+    private BaseLoaderCallback mOpenCVCallBack = new BaseLoaderCallback(this) {
+    	@Override
+    	public void onManagerConnected(int status) {
+    	   switch (status) {
+    	       case LoaderCallbackInterface.SUCCESS:
+    	       {
+    	      Log.i(TAG, "OpenCV loaded successfully");
+    	      // Create and set View
+    	      //setContentView(R.layout.activity_camera);
+    	       } break;
+    	       default:
+    	       {
+    	      super.onManagerConnected(status);
+    	       } break;
+    	   }
+    	    }
+    	};
+    	
+    	
 }
